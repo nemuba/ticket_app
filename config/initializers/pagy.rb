469ebd0c44fadf73b5906a3563e7867bad90a702
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2022-09-26 19:02:35 -0300

    run rubocop -A

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,1 +1,3 @@
-require 'pagy/extras/bootstrap'
+# frozen_string_literal: true
+
+require "pagy/extras/bootstrap"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,7 +8,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-1..100.times do 
+1..100.times do
   Ticket.create(
     title: Faker::Book.title,
     current_status: ["pending", "completed", "removed"].sample,
