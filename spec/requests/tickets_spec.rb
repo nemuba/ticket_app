1ba8cb5b8cab8a2bbfd1f6b80da06830874a4fb8
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2022-09-26 15:23:26 -0300

    create view tickets

--- spec/requests/tickets_spec.rb
+++ spec/requests/tickets_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Homes", type: :request do
+RSpec.describe "Tickets", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/"
+      get "/tickets/index"
       expect(response).to have_http_status(:success)
     end
   end
