3c4f634b6085a88d6e58aaf0a768656b87088cd4
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2022-09-28 18:30:20 -0300

    fix broadcasting tickets of current user

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -11,13 +11,25 @@ class Ticket < ApplicationRecord
   validates :comments, presence: true
   enum :current_status, { pending: "pending", completed: "completed", removed: "removed" }
 
-  after_create_commit { broadcast_prepend_later_to "tickets" }
-  after_update_commit { broadcast_replace_later_to "tickets" }
-  after_destroy_commit { broadcast_remove_to "tickets" }
+  after_create_commit :create_broadcast
+  after_update_commit :update_broadcast
+  after_destroy_commit :destroy_broadcast
 
   def self.by_user(user)
     joins(:user)
       .where(user_id: user.id)
       .order(:created_at)
+  end
+
+  def create_broadcast
+    broadcast_prepend_later_to "tickets_user_#{user_id}"
+  end
+
+  def update_broadcast
+    broadcast_replace_later_to "tickets_user_#{user_id}"
+  end
+
+  def destroy_broadcast
+    broadcast_remove_to "tickets_user_#{user_id}"
   end
 end
